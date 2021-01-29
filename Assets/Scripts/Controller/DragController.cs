b51bbdba813176d28b4a04ca1942c3911d36c20e
Josh Moten <moltenmoustache@hotmail.com>
2021-01-30 09:21:40 +1100

    3D Drag and Drop + DevScene

--- Assets/Scripts/Controller/DragController.cs
+++ Assets/Scripts/Controller/DragController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class DragController : MonoBehaviour
 {
+	#region Singleton
+	static DragController instance;
+	private void Awake()
+	{
+		instance = this;
+	}
+	#endregion Singleton
+
 	[Header("Item")]
 	GameObject selectedItem = null;
 	GameObject highlightedZone = null;
@@ -11,6 +19,14 @@ public class DragController : MonoBehaviour
 	[Header("Dragging Scales")]
 	[SerializeField] float grabbedScaleSpeed = 0.2f;
 	[SerializeField] Vector3 grabbedScale = new Vector3(1.1f, 1.1f, 1.1f);
+
+	[Header("Dragging Stat")]
+	[SerializeField] float pickupTime = 0.25f;
+	public static float PickupTime { get { return instance.pickupTime; } }
+	[SerializeField] float placementTime = 0.2f;
+	public static float PlacementTime { get { return instance.placementTime; } }
+	[SerializeField] float holdDistance = 0.5f;
+	public static float HoldDistance { get { return instance.holdDistance; } }
 
 	// Start is called before the first frame update
 	void Start()
@@ -21,9 +37,7 @@ public class DragController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		MoveHeldItem();
-		HandleClickRaycast();
-		HandleDrop();
+
 	}
 
 	void MoveHeldItem()
--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -9,4 +9,43 @@ public class Item : MonoBehaviour
     public string conditionName;
     public List<NPCType> npcTypes = new List<NPCType>();
     public bool isClaimed = false;
+
+    [Header("Movement")]
+    bool dragging = false;
+    float distance = 0.5f;
+    [SerializeField] float pickupTime = 0.25f;
+    [SerializeField] float placementTime = 0.2f;
+    Vector3 oldPosition;
+
+    void OnMouseDown()
+    {
+        oldPosition = transform.position;
+        dragging = true;
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        int layerMask = 1 << 6;
+
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, ~layerMask))
+		{
+            LeanTween.move(gameObject, hit.point, DragController.PlacementTime);
+		}
+		else
+		{
+            LeanTween.move(gameObject, oldPosition, DragController.PlacementTime);
+		}
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(DragController.HoldDistance);
+            transform.position = rayPoint;
+        }
+    }
 }
