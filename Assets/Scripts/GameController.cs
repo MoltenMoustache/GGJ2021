964f139e75b11173ac3312a981d721aa5a9cc214
Josh Moten <moltenmoustache@hotmail.com>
2021-01-31 13:43:03 +1100

    Game LOOOOP

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 [System.Serializable]
 public struct Day
 {
-    public int peopleThisDay;
+	public int peopleThisDay;
 	public string motd;
 
 	[Range(0, 5)]
@@ -25,11 +26,27 @@ public class GameController : MonoBehaviour
 		instance = this;
 	}
 
-    int currentDay = -1;
-    [SerializeField] List<Day> gameDays = new List<Day>();
+	int currentDay = -1;
+	[SerializeField] List<Day> gameDays = new List<Day>();
 
+	[Header("Game Over")]
+	[SerializeField] GameObject gameOverPanel;
+	[SerializeField] CanvasGroup gameOverCanvasGroup;
+	[SerializeField] TextMeshProUGUI day1Return;
+	[SerializeField] TextMeshProUGUI day1Dismissals;
+	[SerializeField] TextMeshProUGUI day2Return;
+	[SerializeField] TextMeshProUGUI day2Dismissals;
+	[SerializeField] TextMeshProUGUI day3Return;
+	[SerializeField] TextMeshProUGUI day3Dismissals;
+
+	[SerializeField] CanvasGroup returnToMenuCanvasGroup;
+
+	[Header("End of Day")]
+	[SerializeField] GameObject eodPanel;
 	[SerializeField] TextMeshProUGUI dayTextbox;
 	[SerializeField] TextMeshProUGUI motdTextbox;
+	[SerializeField] TextMeshProUGUI successTextbox;
+	[SerializeField] TextMeshProUGUI failTextbox;
 	[SerializeField] CanvasGroup motdCanvasGroup;
 
 	[SerializeField] List<Zone> zones = new List<Zone>();
@@ -41,14 +58,47 @@ public class GameController : MonoBehaviour
 
 	public static void NextDay()
 	{
-		instance.currentDay++;
+		instance.eodPanel.SetActive(false);
 
-		instance.DisplayMessageOfTheDay();
+		instance.currentDay++;
 
 		for (int i = 0; i < instance.zones.Count; i++)
 			instance.zones[i].NextDay(instance.gameDays[instance.currentDay]);
 
 		Debug.Log(instance.currentDay);
+	}
+
+	public static void EndDay()
+	{
+		ScoreHandler.EndDay();
+		if (instance.currentDay == instance.gameDays.Count - 1)
+		{
+			instance.GameOver();
+		}
+		else
+		{
+			instance.eodPanel.SetActive(true);
+			instance.DisplayMessageOfTheDay();
+			LeanTween.alphaCanvas(instance.motdCanvasGroup, 1, 2.5f).setOnComplete(() => LeanTween.alphaCanvas(instance.motdCanvasGroup, 0, 2.5f).setDelay(4.0f).setOnComplete(() => NextDay()));
+		}
+	}
+
+	void GameOver()
+	{
+		gameOverPanel.SetActive(true);
+
+		day1Return.text = ScoreHandler.GetDayTally(0, true).ToString();
+		day1Dismissals.text = ScoreHandler.GetDayTally(0, false).ToString();
+		day2Return.text = ScoreHandler.GetDayTally(1, true).ToString();
+		day2Dismissals.text = ScoreHandler.GetDayTally(1, false).ToString();
+		day3Return.text = ScoreHandler.GetDayTally(2, true).ToString();
+		day3Dismissals.text = ScoreHandler.GetDayTally(2, false).ToString();
+		LeanTween.alphaCanvas(gameOverCanvasGroup, 1, 3.0f);
+	}
+
+	public void ReturnToMenu()
+	{
+		LeanTween.alphaCanvas(returnToMenuCanvasGroup, 1, 2.0f).setOnComplete(() => SceneManager.LoadScene(0));
 	}
 
 
@@ -61,13 +111,14 @@ public class GameController : MonoBehaviour
 	public void DisplayMessageOfTheDay()
 	{
 		instance.dayTextbox.text = "Day " + (int)(instance.currentDay + 1);
-		instance.motdTextbox.text = instance.gameDays[instance.currentDay].motd;
-		LeanTween.alphaCanvas(instance.motdCanvasGroup, 1, 2.0f).setFrom(0).setOnComplete(() => LeanTween.value(0, 1, 8.0f).setOnComplete(() => LeanTween.alphaCanvas(instance.motdCanvasGroup, 0, 2.0f).setFrom(1)));
+		//instance.motdTextbox.text = instance.gameDays[instance.currentDay].motd;
+		successTextbox.text = ScoreHandler.GetDayTally(currentDay, true).ToString();
+		failTextbox.text = ScoreHandler.GetDayTally(currentDay, false).ToString();
 	}
 
 	IEnumerator StartGame()
 	{
-		yield return new WaitForSeconds(5);
+		yield return new WaitForSeconds(0);
 		NextDay();
 	}
 }
--- Assets/Scripts/ScoreHandler.cs
+++ Assets/Scripts/ScoreHandler.cs
@@ -6,6 +6,9 @@ public static class ScoreHandler
 {
     static int numberOfSuccess;
     static int numberOfFail;
+    static List<int> dailySuccess = new List<int>();
+    static List<int> dailyFail = new List<int>();
+
 
     public static void SubmitRequest(bool wasSuccess)
 	{
@@ -13,6 +16,23 @@ public static class ScoreHandler
             numberOfSuccess++;
         else
             numberOfFail++;
+	}
+
+    public static void EndDay()
+	{
+        dailySuccess.Add(numberOfSuccess);
+        dailyFail.Add(numberOfFail);
+
+        numberOfSuccess = 0;
+        numberOfFail = 0;
+	}
+
+    public static int GetDayTally(int dayIndex, bool getSuccess)
+	{
+        if (getSuccess)
+            return dailySuccess[dayIndex];
+        else
+            return dailyFail[dayIndex];
 	}
 
     public static void ResetValues()
